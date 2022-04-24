d5268c9a78ca78ab4d692c8985620bd723659477
Milos <m_k0vacev1c@hotmail.com>
2022-04-24 18:54:45 +0200

    Delete backend directory

--- backend/src/main/java/com/mk0vacev1c/employeemanager/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mk0vacev1c.employeemanager.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
